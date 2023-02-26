d066bbd6e78eb77567bd1fc5d46460e4ed574a35
Lucas dos Santos <ldsantos@minsait.com>
2023-02-26 20:04:20 -0300

    adc form de clientes

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente{
+  id:number;
+  nome: string;
+  cpf:string;
+  celular:string;
+  email:string;
+  dtNascimento:string;
+  sexo:string;
+  endereco:string
+  dataCadastro:string
+}
