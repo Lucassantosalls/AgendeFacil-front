91d1315d12a47b14dd5cfb5f4f4266834e3ff4f1
Lucas dos Santos <ldsantos@minsait.com>
2023-03-12 10:49:22 -0300

    finalizando cadastro de clientes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{Cliente} from '../cliente'
+import{ ClientesService } from '../../clientes.service'
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -9,10 +12,61 @@ import{Cliente} from '../cliente'
 export class ClientesFormComponent implements OnInit {
 
   cliente:Cliente;
+  sucesso: boolean = false;
+  erros: String[];
+  id: number;
 
-  constructor() { }
+  constructor(
+    private service: ClientesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+    this.cliente = new Cliente();
+   }
 
   ngOnInit(): void {
+    let params =  this.activatedRoute.params
+    params.subscribe(urlParams =>{
+      this.id = urlParams['id'];
+      if(this.id){
+        this.service.getClienteById(this.id)
+        .subscribe(
+          response => this.cliente = response,
+          errorResponse => this.cliente = new Cliente()
+        )
+      }
+    })
+  }
+
+  voltarConsulta(){
+    this.router.navigate(['/clientes-lista'])
+  }
+
+  onSubmit(){
+
+    if(this.id){
+
+      this.service
+      .atualizar(this.cliente)
+      .subscribe(response =>{
+        this.sucesso = true;
+        this.erros = null;
+      },errorResponse =>{
+        this.erros = ['Erro ao atualizar cliente.']
+      })
+
+    }else{
+      this.service
+      .salvar(this.cliente)
+      .subscribe(response =>{
+        this.sucesso = true;
+        this.erros = null;
+        this.cliente = response;
+      }, errorResponse => {
+        this.sucesso = false
+        this.erros = errorResponse.error.erros;
+      }
+      )
+    }
   }
 
 }
--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 
 const routes: Routes = [
-  { path:'clientes-cadastro',component: ClientesFormComponent}
+  { path:'clientes-cadastro',component: ClientesFormComponent},
+  { path:'clientes-cadastro/:id',component: ClientesFormComponent},
+  { path:'clientes-lista',component: ClientesListaComponent }
 ];
 
 @NgModule({
