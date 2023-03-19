2939e4a74bea47ab00429df8485fbe0e58f46cee
Lucas dos Santos <ldsantos@minsait.com>
2023-03-19 13:51:18 -0300

    adicao do modulo de servicos

---
+++ src/app/servico-prestado/servico-prestado-lista/servicoPrestadoBusca.ts
@@ -0,0 +1,11 @@
+import { Cliente } from "src/app/clientes/cliente";
+
+export class ServicoPrestadoBusca {
+  descricao: string;
+  valor: number;
+  dataDoServico: string;
+  cliente: Cliente;
+  resumo: string;
+  enderecoServico: string;
+  concluido: string;
+}
---
+++ src/app/servico-prestado/servicoPrestado.ts
@@ -0,0 +1,9 @@
+export class ServicoPrestado {
+  descricao:String;
+  resumo: String;
+  id_cliente:number;
+  enderecoServico: String;
+  valor: String;
+  concluido: String;
+  dataDoServico: String;
+}
